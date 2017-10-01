dc303a7b62bc304b8d02c7516ec8b2c22c2a5803
sourcx <wh@y42.nl>
2017-10-01 15:24:30 +0200

    init

---
+++ hoots/hoots/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hoots
+//
+//  Created by frank on 01/10/2017.
+//  Copyright © 2017 gloryhammer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hoots/hoots/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hoots
+//
+//  Created by frank on 01/10/2017.
+//  Copyright © 2017 gloryhammer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

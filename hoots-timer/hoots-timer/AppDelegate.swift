7ea5a9c868efae0af792c2c4bef640ceaa3054a3
sourcx <wh@y42.nl>
2017-10-02 11:55:56 +0200

    Init hoots-timer.

---
+++ hoots-timer/hoots-timer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hoots-timer
+//
+//  Created by frank on 02/10/2017.
+//  Copyright © 2017 gloryhammer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
--- hoots/hoots/ViewController.swift
+++ hoots/hoots/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.`
         optionals()
     }
 
